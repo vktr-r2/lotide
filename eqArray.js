6f1d4117858e048195ef73311221370d6dcd8618
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-04 22:49:53 -0400

    Initial Commit: Add eqArray.js (Function designed to compare two arrays and return true if they're identical, false if they are not

---
+++ eqArray.js
@@ -0,0 +1,23 @@
+//Define assertEqual function, takes two params
+const assertEqual = (actual, expected) => {
+  //If statement checks if two params are identical
+  if (actual !== expected) {
+    //If FALSE, log error
+    console.log(`🔴 🔴 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    //If TRUE, log success
+    console.log(`🟩 🟩 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "cat", 8, ' ', ''  ], ["1", "2", "cat", 8, ' ']), true);
