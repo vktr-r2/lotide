d2da83c86495e8b18074144966ca708717877f9f
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-05 19:21:52 -0400

    Revert back to ba19161

--- countLetters.js
+++ countLetters.js
@@ -12,31 +12,25 @@ const assertEqual = (actual, expected) => {
 //TASK//
 
 //The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
-//For example, lettersCounter('LHL') should return results indicating that L appears twice, and H once.
+//For example, countLetters('LHL') should return results indicating that L appears twice, and H once.
 //this function could return an object where each unique character encountered in the string is a property of the object and the value for that property/key should be the number of occurrences for that character.
 
 //HOW//
 //How do I handle letters that I am supposed to check for, but don't appear in my string?
 
-
-const lettersCounter = (checkLetters) => {
-  const letterCountObject = {}
-  for (check of checkLetters) {
-    letterCountObject[check] = 0
+//countLetters takes a inputString and checkLetters array
+const countLetters = (inputString, checkLetters) => {
+  const letterCount = {}
+  for (letter of checkLetters) {
+    if (letter !== ' ')letterCount[letter] = 0
   }
-  console.log(letterCountObject);
-  return letterCountObject;
+  console.log(letterCount)
 };
 
-const stringChecker = (inputString, letterCountObject) => {
-  console.log(letterCountObject);
-  return letterCountObject;
-}
 
 //TEST CODE//
 
 const string = "I am slowly getting very sleep";
-const checkFor = ['a', 'b', 'e', 's'];
+const check = ['a', 'b', 'e', 's'];
 
-lettersCounter(checkFor);
-stringChecker(string);
+countLetters(string,check);
