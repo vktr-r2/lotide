bee8953f3a79335bf906935a90d6ce9cc3d75d41
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-05 14:35:57 -0400

    Add test code lines and switch Math.ceil to Math.floor on line 63 to fix bug after testing

--- middle.js
+++ middle.js
@@ -42,28 +42,32 @@ const assertArraysEqual = (array1, array2) => {
 //Declare middle function which takes array as parameter
 const middle = (arrayArg) => {
   //Declare midPoints empty array to store final result
-  let midPoints = []
+  let midPoints = [];
   //Declare arrayLength as number value to store the length of the array passed into the function
   let arrayLength = arrayArg.length;
   //Return empty array if arrayLength > 3 (No midpoint)
-  if (arrayLength < 3) {return midPoints;}
+  if (arrayLength < 3) {
+    return midPoints;
+  }
   //Return array of two elements if arrayLength is even number
   if (arrayLength % 2 === 0) {
     //First midpoint equals arrayArg index at arrayLength divided by 2 then subtracted by one (making up for 0 indexing)
-    midPoints.push(arrayArg[(arrayLength/2) - 1]);
+    midPoints.push(arrayArg[(arrayLength / 2) - 1]);
     //Second midpoint equals arrayArg index at arrayLength divided by 2
-    midPoints.push(arrayArg[arrayLength/2]);
+    midPoints.push(arrayArg[arrayLength / 2]);
     return midPoints;
   }
   //Return array of one element if arrayLength is odd number
   if (arrayLength % 2 === 1) {
-    //Midpoint equals arrayLength divded by two then rounded up
-    midPoints.push(arrayArg[Math.ceil(arrayLength/2)]);
+    //Midpoint equals arrayArg index of arrayLength divded by 2, then rounded down (round down to make up for 0 indexing)
+    midPoints.push(arrayArg[Math.floor(arrayLength / 2)]);
     return midPoints;
   }
-}; 
-
-
+};
 
 
 //TEST CODE
+
+assertArraysEqual(middle([1,2]), []);
+assertArraysEqual(middle([1,2,3,4,5]), [3]);
+assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
