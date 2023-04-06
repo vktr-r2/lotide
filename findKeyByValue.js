00139837f72ae2a4e36b5129877f9c7a01342a97
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-05 23:06:13 -0400

    Add assertEqual function for testing

---
+++ findKeyByValue.js
@@ -0,0 +1,10 @@
+const assertEqual = (actual, expected) => {
+  //If statement checks if two params are identical
+  if (actual !== expected) {
+    //If FALSE, log error
+    console.log(`🔴 🔴 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    //If TRUE, log success
+    console.log(`🟩 🟩 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
