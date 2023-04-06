c91569c39e06e22e38ca2bec1e4ff1a8140bb231
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-06 09:48:14 -0400

    Declare empty eqObjects function and add assert functions and test code for testing

---
+++ eqObjects.js
@@ -0,0 +1,34 @@
+//Define assertEqual function, takes two params
+const assertEqual = (actual, expected) => {
+  //If statement checks if two params are identical
+  if (actual !== expected) {
+    //If FALSE, log error
+    console.log(`🔴 🔴 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    //If TRUE, log success
+    console.log(`🟩 🟩 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = (object1, object2) => {
+
+};
+
+
+
+
+
+
+//TEST CODE//
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+eqObjects(shirtObject , anotherShirtObject); // => true
+assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject , longSleeveShirtObject); // => false
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject), true);
