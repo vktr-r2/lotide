ea83245e8d7c98ab108e3274a805e6abed1aeb32
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-05 22:33:32 -0400

    Add test code and assertion functions

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,20 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+//Define assertArraysEqual func, takes two arrays as params
+const assertArraysEqual = (array1, array2) => {
+  //Declare assertion var to equal the output of two arrays being passed into eqArrays function
+  let assertion = eqArrays(array1, array2);
+  //Console log pass or fail accordingly
+  if (assertion === true) console.log(`🟩 🟩 Assertion Passed: array1 === array2`);
+  if (assertion === false) console.log(`🔴 🔴 Assertion Failed: array1 !== array2`);
+};
+
 const letterPositions = function(sentence) {
   const results = {};
     for (i = 0; i < sentence.length; i++) {
@@ -13,8 +30,6 @@ const letterPositions = function(sentence) {
     }
   return results;
 };
-
-
 
 
 letterPositions("hello");
