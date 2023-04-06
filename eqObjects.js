e3f4c7208a168c2621118126426cd7960263edd6
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-06 14:12:00 -0400

    Implemented functionality to check if object properties are both arrays, and if so compare the two using eqArrays function.  All tests passed

--- eqObjects.js
+++ eqObjects.js
@@ -20,13 +20,21 @@ const eqArrays = (array1, array2) => {
 
 //eqObjects function checks whether two objects are identical to one another
 const eqObjects = (object1, object2) => {
+  //Check to see if Object.keys array lengths are equal
   const Object1Keys = Object.keys(object1);
   const Object2Keys = Object.keys(object2);
   if (Object1Keys.length !== Object2Keys.length) {
     return false;
   }
+  //Iterate through each propery with for..in
   for (property in object1) {
-    if (object1[property] !== object2[property]) {
+      //Check if value is an array, if TRUE run nested if statement holding eqArrays function to compare arrays
+      if (Array.isArray(object1[property]) && Array.isArray(object2[property])) {
+        if (!eqArrays(object1[property],object2[property])) {
+          return false;
+        }
+        //If isArray false, compare two properties with inequality opperator
+      } else if (object1[property] !== object2[property]) {
       return false;
     }
   }
@@ -34,13 +42,9 @@ const eqObjects = (object1, object2) => {
 };
 
 
-
-
-
-
 //TEST CODE//
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+const anotherMultiColorShirtObject = { colors: ["red", "blue"], size:"medium" };
 eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
 assertEqual(eqObjects(multiColorShirtObject , anotherMultiColorShirtObject), true);
 
