91e043f34d23faea371076f18765ca944d1983e4
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-07 01:47:40 -0400

    Test passed, eslint completed

--- findKey.js
+++ findKey.js
@@ -24,22 +24,21 @@ const assertEqual = (actual, expected) => {
 
 const findKey = (object, callback) => {
   for (let key in object) {
-    //let value = object[key];
     if (callback(object[key])) {
-      //console.log(`Value: ${value}`)
-      //console.log(`Struggling with: ${key}`);
-      console.log(key);
+      return (key);
     }
   }
 };
 
 
+assertEqual(
+  findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2), "noma");
 
-findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+
