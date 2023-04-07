86ed7ba9ecbc77bc0715783260e95a374e15ae69
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-06 23:32:37 -0400

    Add maps.js file, implement our own map function

---
+++ map.js
@@ -0,0 +1,13 @@
+const words = ["my", "first", "callback", "function"];
+
+const map = (array, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
