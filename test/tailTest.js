d4e3f74aa081d2f6e8844fafeb8bc51ba26dc203
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-10 18:45:20 -0400

    Remove all previous test code

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,3 @@
-// const assertArraysEqual = require('../assertArraysEqual');
-// const tail = require('../tail');
-
-// const fullArray = [1,2,3];
-// assertArraysEqual(tail(fullArray),[2,3]);
-
 const assert = require('chai').assert;
 const tail = require('../tail');
 
