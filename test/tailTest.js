194bf155a665e0872fa3e6ae6c39eff185f82c88
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-10 14:44:56 -0400

    Add tailTest.js to repo > tests tail.js functionality

---
+++ test/tailTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const tail = require('../tail');
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+assertArraysEqual(tail(words),["Lighthouse", "Labs"]);
