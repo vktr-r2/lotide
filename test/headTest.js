6e7b57460abd87dd2469e6d1f50e699f3dc03f95
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-10 18:46:13 -0400

    Remove all previous test code

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,3 @@
-// const assertEqual = require('../assertEqual');
-// const head = require('../head');
-
-// assertEqual(head([5,6,7]), 5);  //PASSED
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //PASSED
-// assertEqual(head([])); //UNDEFINED === UNDEFINED PASSED
-// assertEqual(head([1]), 1); //PASSED
-
 const assert = require('chai').assert;
 const head   = require('../head');
 
