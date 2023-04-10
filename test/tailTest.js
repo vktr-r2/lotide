cc8f2c861bb06b65ab59e75bd382c626582e9a1d
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-10 18:42:52 -0400

    Convert to mocha and chai test using assert.deepEqual

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,14 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const tail = require('../tail');
+
+// const fullArray = [1,2,3];
+// assertArraysEqual(tail(fullArray),[2,3]);
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertArraysEqual(tail(words),["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1,2,3]), [2, 3]);
+  });
+});
