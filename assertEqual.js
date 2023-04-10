d2e4eece3d36da421835e42c2867877714fc76fd
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-10 14:12:22 -0400

    Exported function

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,11 @@
-//Define assertEqual function, takes two params
-const assertEqual = (actual, expected) => {
-  //If statement checks if two params are identical
-  if (actual !== expected) {
-    //If FALSE, log error
+
+const assertEqual = function(actual, expected) {
+  if (!(actual === expected)) {
     console.log(`🔴 🔴 Assertion Failed: ${actual} !== ${expected}`);
   } else {
-    //If TRUE, log success
     console.log(`🟩 🟩 Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
