d7e0eeb64e271bd0a079f014141706115dcb2b86
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-10 18:54:41 -0400

    Covert to test using mocha and chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,19 @@
+// const assertEqual = require('../assertEqual');
+
+// assertEqual(2, 2);
+// assertEqual(1, 2);
+// assertEqual("2", 2);
+
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual(2, 2);
-assertEqual(1, 2);
-assertEqual("2", 2);
+describe("#assertEqual", () => {
+
+  it("returns true for 1 === 1", () => {
+    assert.strictEqual(assertEqual(1, 1), true);
+  });
+
+  it(`returns false for 1 === "1"`, () => {
+    assert.strictEqual(assertEqual(1, "1"), false); 
+  });
+});
