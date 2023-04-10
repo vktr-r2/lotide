0bb35a109e061f01660be13e4a5e5ba176419004
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-10 14:13:20 -0400

    Exported function

--- head.js
+++ head.js
@@ -1,22 +1,7 @@
-//Define assertEqual function, takes two params
-const assertEqual = (actual, expected) => {
-  //If statement checks if two params are identical
-  if (actual !== expected) {
-    //If FALSE, log error
-    console.log(`🔴 🔴 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    //If TRUE, log success
-    console.log(`🟩 🟩 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 //Define head function which takes array as param
 const head = (array) => {
   //Return the value in 0 index position of array
   return array[0];
 };
 
-//assertEqual(head([5,6,7]), 5);  PASSED
-//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); PASSED
-//assertEqual(head([])); UNDEFINED === UNDEFINED PASSED
-//assertEqual(head([1]), 1); PASSED
+module.exports = head;
