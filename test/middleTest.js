8035d55187de702ff14b0c056ab646907a72c9f9
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-10 18:59:11 -0400

    Conver to use mocha and chai for testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
+describe("#middle test", () => {
+
+  it("returns [] for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+  
+  it("returns [3] for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+
+  it("returns [4,5] for [1,2,3,4,5,6,7,8]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
+  });
+});
